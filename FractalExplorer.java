d505035536d6d7155ba0ef84421df52cab8f5684
Vaseem Naazleen Shaik <vnaazleen@users.noreply.github.com>
2021-01-12 21:28:38 +0530

    Added up down right left keys

--- FractalExplorer.java
+++ FractalExplorer.java
@@ -6,159 +6,208 @@
 import java.util.*;
 import java.awt.*;
 import java.awt.image.*;
+import java.security.Key;
 import java.awt.event.*;
 import javax.swing.*;
 
 
-
 public class FractalExplorer extends JFrame {
+	
+	static final int WIDTH  = 600;
+	static final int HEIGHT = 600;
+	
+	Canvas canvas;
+	BufferedImage fractalImage;
+	
+	static final int MAX_ITER = 200;
+	
+	static final double DEFAULT_ZOOM       = 100.0;
+	static final double DEFAULT_TOP_LEFT_X = -3.0;
+	static final double DEFAULT_TOP_LEFT_Y = +3.0;
+	
+	double zoomFactor = DEFAULT_ZOOM;
+	double topLeftX   = DEFAULT_TOP_LEFT_X;
+	double topLeftY   = DEFAULT_TOP_LEFT_Y;
 
-    static final int WIDTH = 600;
-    static final int HEIGHT = 600;
+// -------------------------------------------------------------------
+	public FractalExplorer() {
+		setInitialGUIProperties();
+		addCanvas();
+		canvas.addKeyStrokeEvents();
+		updateFractal();
+		this.setVisible(true);
+	}
+	
+// -------------------------------------------------------------------
 
-    static final int MAX_ITER = 200;
+	public static void main(String[] args) {
+		new FractalExplorer();
+	}
+	
+// -------------------------------------------------------------------
 
-    static final double DEFAULT_ZOOM = 100.0;
-    static final double  DEFAULT_TOP_LEFT_X = -3.0;
-    static final double  DEFAULT_TOP_LEFT_Y = +3.0;
-    
-    
-    double zoomFactor = DEFAULT_ZOOM;
-    double topLeftX   = DEFAULT_TOP_LEFT_X;
-    double topLeftY   = DEFAULT_TOP_LEFT_Y;
+	private void addCanvas() {
 
-    Canvas canvas;
-    BufferedImage fractalImage;
+		canvas = new Canvas();
+		fractalImage = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+		canvas.setVisible(true);
+		this.add(canvas, BorderLayout.CENTER);
 
-    public FractalExplorer() {
-        setInitialGUIProperties();
-        addCanvas();
-        canvas.addKeyStrokeEvents();
-        updateFractal();
-    }
+	} // addCanvas
 
-    public static void main(String[] args) {
-        new FractalExplorer();
-    }
-
-
-    private void updateFractal() {
-
-        for (int x = 0; x < WIDTH; x++) {
-            for (int y = 0; y < HEIGHT; y++) {
-
-                double c_real = getXPos(x);
-                double c_img = getYPos(y);
-
-                int iterCount = computeIterations(c_real, c_img);
-
-                int pixelColor = makeColor(iterCount);
-                fractalImage.setRGB(x, y, pixelColor);
-
-            }
-        }
-
-        canvas.repaint();
-    }
-
-    private int makeColor(int iterCount) {
-
-        int color = 0b10111010101010111010011; 
-        int mask  = 0b000000000000010101110111; 
-
-        int shiftMag = iterCount / 13;
-
-        if (iterCount == MAX_ITER) {
-            return Color.BLACK.getRGB();
-        }
-
-        return color | (mask << shiftMag);
-    }
-
-    private double getXPos(int x) {
-        return x / zoomFactor + topLeftX;
-    }
-
-    private double getYPos(int y) {
-        return y / zoomFactor - topLeftY;
-    }
-
-    private int computeIterations(double c_real, double c_img) {
-        /*
-            Let c = c_real + c_img
-            Let z = z_real + z_img
-
-            z' =  z * z + c
-               = (z_real + z_img)(z_real + z_img) + c_real + c_img
-               = z_real ** 2 + 2 * z_real * z_img - z_img ** 2 + c_real + c_img
-            
-            z_real' = z_real ** 2 - z_img ** 2 + c_real
-            z_img' = 2 * z_real * z_img + c_img
-        */
-
-        double z_real = 0.0;
-        double z_img = 0.0;
-
-        int iterCount = 0;
-
-        // calculating modulus or dist from origin  √(a**2 + b**2) <= 2.0 ==> (a**2 + b**2) <= (2.0) ** 2
-
-        while ( z_real * z_real + z_img * z_img <= 4.0) {
-            double z_real_temp = z_real;
-
-            z_real = z_real * z_real - z_img * z_img + c_real;
-            z_img = 2 * z_real_temp * z_img + c_img;
-
-            if (iterCount >= MAX_ITER) {
-                return MAX_ITER;
-            }
-
-            iterCount++;
-
-        }
-
-        return iterCount;
-    }
-
-    public void setInitialGUIProperties() {
-
-        // properties of window
-        this.setTitle("Fractal Explorer");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(WIDTH, HEIGHT);
-        this.setResizable(false);
-        this.setLocationRelativeTo(null);
-        this.setVisible(true);
-
-    }
-
-    private void addCanvas() {
-
-        canvas = new Canvas();
-        fractalImage = new BufferedImage(HEIGHT, WIDTH, BufferedImage.TYPE_INT_RGB);
-        canvas.setVisible(true);
-        this.add(canvas, BorderLayout.CENTER);
-    
-    }
-
-
-    public void adjustZoom( double newX, double newY, double newZoomFactor ) {
+// -------------------------------------------------------------------
 		
-            topLeftX += newX/zoomFactor;
-            topLeftY -= newY/zoomFactor;
-            
-            zoomFactor = newZoomFactor;
-            
-            topLeftX -= ( WIDTH/2) / zoomFactor;
-            topLeftY += (HEIGHT/2) / zoomFactor;
-            
-            updateFractal();
-            
-    } // adjustZoom
+		private void setInitialGUIProperties() {
+			
+			this.setTitle("Fractal Explorer");
+			this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			this.setSize(WIDTH, HEIGHT);
+			this.setResizable(false);
+			this.setLocationRelativeTo(null);
+			
+		} // setInitialGUIProperties
 
-    private class Canvas extends JPanel implements MouseListener {
+// -------------------------------------------------------------------
+	private double getXPos(double x) {
+		return x/zoomFactor + topLeftX;
+	} // getXPos
+// -------------------------------------------------------------------
+	private double getYPos(double y) {
+		return y/zoomFactor - topLeftY;
+	} // getYPos
+// -------------------------------------------------------------------
+	
+	/**
+	 * Updates the fractal by computing the number of iterations
+	 * for each point in the fractal and changing the color
+	 * based on that.
+	 **/
+	
+	public void updateFractal() {
+		
+		for (int x = 0; x < WIDTH; x++ ) {
+			for (int y = 0; y < HEIGHT; y++ ) {
+				
+				double c_r = getXPos(x);
+				double c_i = getYPos(y);
+				
+				int iterCount = computeIterations(c_r, c_i);
+				
+				int pixelColor = makeColor(iterCount);
+				fractalImage.setRGB(x, y, pixelColor);
+				
+			}
+		}
+		
+		canvas.repaint();
+		
+	} // updateFractal
+// -------------------------------------------------------------------	
+	/** Returns a posterized color based off of the iteration count
+	    of a given point in the fractal **/
+	private int makeColor( int iterCount ) {
+		
+		int color = 0b10111010101010111010011; 
+		int mask  = 0b000000000000010101110111; 
+		int shiftMag = iterCount / 13;
+		
+		if (iterCount == MAX_ITER) 
+			return Color.BLACK.getRGB();
+		
+		return color | (mask << shiftMag);
+		
+	} // makeColor
 
-        public Canvas() {
+// -------------------------------------------------------------------
+
+	private int computeIterations(double c_r, double c_i) {
+		
+		/*
+		
+		Let c = c_r + c_i
+		Let z = z_r + z_i
+		
+		z' = z*z + c
+		   = (z_r + z_i)(z_r + z_i) + (c_r + c_i)
+		   = z_r² + 2*z_r*z_i - z_i² + c_r + c_i
+
+		     z_r' = z_r² - z_i² + c_r
+		     z_i' = 2*z_i*z_r + c_i
+		     
+		*/
+
+		double z_r = 0.0;
+		double z_i = 0.0;
+		
+		int iterCount = 0;
+
+		// Modulus (distance) formula:
+		// √(a² + b²) <= 2.0
+		// a² + b² <= 4.0
+		while ( z_r*z_r + z_i*z_i <= 4.0 ) {
+			
+			double z_r_tmp = z_r;
+			
+			z_r = z_r*z_r - z_i*z_i + c_r;
+			z_i = 2*z_i*z_r_tmp + c_i;
+			
+			// Point was inside the Mandelbrot set
+			if (iterCount >= MAX_ITER) 
+				return MAX_ITER;
+			
+			iterCount++;
+			
+		}
+		
+		// Complex point was outside Mandelbrot set
+		return iterCount;
+		
+	} // computeIterations
+// -------------------------------------------------------------------
+	private void moveUp() {
+		double curHeight = HEIGHT / zoomFactor;
+		topLeftY -= curHeight / 6;
+		updateFractal();
+	} // moveUp
+// -------------------------------------------------------------------
+	private void moveDown() {
+		double curHeight = HEIGHT / zoomFactor;
+		topLeftY += curHeight / 6;
+		updateFractal();
+	} // moveDown
+// -------------------------------------------------------------------
+	private void moveLeft() {
+		double curWidth = WIDTH / zoomFactor;
+		topLeftX += curWidth / 6;
+		updateFractal();
+	} // moveLeft
+// -------------------------------------------------------------------
+	private void moveRight() {
+		double curWidth = WIDTH / zoomFactor;
+		topLeftX -= curWidth / 6;
+		updateFractal();
+	} // moveRight
+// -------------------------------------------------------------------		
+
+	private void adjustZoom( double newX, double newY, double newZoomFactor ) {
+		
+		topLeftX += newX/zoomFactor;
+		topLeftY -= newY/zoomFactor;
+		
+		zoomFactor = newZoomFactor;
+		
+		topLeftX -= ( WIDTH/2) / zoomFactor;
+		topLeftY += (HEIGHT/2) / zoomFactor;
+		
+		updateFractal();
+		
+	} // adjustZoom
+
+// -------------------------------------------------------------------	
+	
+	private class Canvas extends JPanel implements MouseListener {
+		
+		public Canvas() {
 			addMouseListener(this);
 		} 
 		
@@ -169,8 +218,8 @@ public class FractalExplorer extends JFrame {
 		@Override public void paintComponent(Graphics drawingObj) {
 			drawingObj.drawImage( fractalImage, 0, 0, null );
 		} // paintComponent
-
-        @Override public void mousePressed(MouseEvent mouse) {
+		
+		@Override public void mousePressed(MouseEvent mouse) {
 			
 			double x = (double) mouse.getX();
 			double y = (double) mouse.getY();
@@ -189,12 +238,56 @@ public class FractalExplorer extends JFrame {
 				
 			}
 			
-		} 
-
-        @Override public void mouseReleased(MouseEvent mouse) {}
-        @Override public void mouseClicked(MouseEvent mouse) {}
-        @Override public void mouseEntered(MouseEvent mouse) {}
-        @Override public void mouseExited(MouseEvent mouse) {}
-
-    }
-}
+		} // mousePressed
+		
+		public void addKeyStrokeEvents() {
+			
+			KeyStroke uKey = KeyStroke.getKeyStroke(KeyEvent.VK_U, 0 );
+			KeyStroke dKey = KeyStroke.getKeyStroke(KeyEvent.VK_D, 0 );
+			KeyStroke lKey = KeyStroke.getKeyStroke(KeyEvent.VK_L, 0 );
+			KeyStroke rKey = KeyStroke.getKeyStroke(KeyEvent.VK_R, 0 );
+			
+			Action uPressed = new AbstractAction() {
+				@Override public void actionPerformed(ActionEvent e) {
+					moveUp();
+				}
+			};
+			
+			Action lPressed = new AbstractAction() {
+				@Override public void actionPerformed(ActionEvent e) {
+					moveLeft();
+				}
+			};
+			
+			Action dPressed = new AbstractAction() {
+				@Override public void actionPerformed(ActionEvent e) {
+					moveDown();
+				}
+			};
+			
+			Action rPressed = new AbstractAction() {
+				@Override public void actionPerformed(ActionEvent e) {
+					moveRight();
+				}
+			}; 
+			
+			this.getInputMap().put( uKey, "u_key" );
+			this.getInputMap().put( rKey, "r_key" );
+			this.getInputMap().put( lKey, "l_key" );
+			this.getInputMap().put( dKey, "d_key" );
+			
+			this.getActionMap().put( "u_key", uPressed );
+			this.getActionMap().put( "r_key", rPressed );
+			this.getActionMap().put( "l_key", lPressed );
+			this.getActionMap().put( "d_key", dPressed );
+			
+		} // addKeyStrokeEvents
+		
+		@Override public void mouseReleased(MouseEvent mouse){ }
+		@Override public void mouseClicked(MouseEvent mouse) { }
+		@Override public void mouseEntered(MouseEvent mouse) { }
+		@Override public void mouseExited (MouseEvent mouse) { }
+		
+	} // Canvas
+	
+} // FractalExplorer
