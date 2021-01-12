a0386685ccc82754a1131947cbd5c2a0bb9e6fc3
Vaseem Naazleen Shaik <vnaazleen@users.noreply.github.com>
2021-01-12 12:23:33 +0530

    Initial window set up

---
+++ FractalExplorer.java
@@ -0,0 +1,36 @@
+/**
+ * A Fractal Explorer created in Java
+ * @author Vaseem Naazleen Shaik, shaikvaseemnaazleen@gmail.com
+ **/
+
+import java.util.*;
+import java.awt.*;
+import java.awt.image.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class FractalExplorer extends JFrame{
+
+    static final int WIDTH = 600;
+    static final int HEIGHT = 600;
+
+    public FractalExplorer() {
+        setInitialGUIProperties();
+    }
+
+    public void setInitialGUIProperties() {
+
+        // properties of window
+        this.setTitle("Fractal Explorer");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(WIDTH, HEIGHT);
+        this.setResizable(false);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        new FractalExplorer();
+    }
+}
