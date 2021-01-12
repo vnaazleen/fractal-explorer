7f356c41c301dd5f26acfbbc33d3e2f8e5571480
Vaseem Naazleen Shaik <vnaazleen@users.noreply.github.com>
2021-01-12 12:33:31 +0530

    Added jpanel to canvas

--- FractalExplorer.java
+++ FractalExplorer.java
@@ -14,8 +14,12 @@ public class FractalExplorer extends JFrame{
     static final int WIDTH = 600;
     static final int HEIGHT = 600;
 
+    Canvas canvas;
+    BufferedImage fractalImage;
+
     public FractalExplorer() {
         setInitialGUIProperties();
+        addCanvas();
     }
 
     public void setInitialGUIProperties() {
@@ -30,7 +34,30 @@ public class FractalExplorer extends JFrame{
 
     }
 
+    private void addCanvas() {
+
+        canvas = new Canvas();
+        fractalImage = new BufferedImage(HEIGHT, WIDTH, BufferedImage.TYPE_INT_RGB);
+        canvas.setVisible(true);
+        this.add(canvas, BorderLayout.CENTER);
+
+    }
+
     public static void main(String[] args) {
+
         new FractalExplorer();
+
+    }
+
+    private class Canvas extends JPanel {
+
+        @Override public Dimension getPreferredSize() {
+            return new Dimension(WIDTH, HEIGHT);
+        }
+
+        @Override public void paintComponent(Graphics drawingObj) {
+            drawingObj.drawImage(fractalImage, 0, 0, null);
+        }
+
     }
 }
